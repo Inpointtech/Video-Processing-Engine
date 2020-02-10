be2ced643e7badf23b01f1173a3d575c49b4b967
akshayinpoint <akshay@inpointtech.com>
2020-02-10 19:53:36 +0530

    Refactor code and add support for face detection
    Note: XA
    * Refactor code.
    * Add support for face detection (commented out).
    * Reduce cv2.waitKey delay to 1 sec.

--- run.py
+++ run.py
@@ -5,21 +5,24 @@ import time
 import cv2
 import numpy
 
+from video_processing_engine.utils.common import toast
 from video_processing_engine.utils.opencv import (disconnect,
                                                   draw_box_with_tuple, rescale)
-from video_processing_engine.utils.common import toast
+from video_processing_engine.core.redact.faces import detect_faces
 
 while True:
   stream = cv2.VideoCapture(0)
-  time.sleep(3.0)
   toast('Success', 'Stream started.')
+  time.sleep(2.0)
   # Keep the service running.
   try:
     while stream.isOpened():
       _, frame = stream.read()
       frame = rescale(frame, width=400)
-      # Terminate the application after pressing 'Esc' key.
-      if cv2.waitKey(5) & 0xFF == int(27):
+      # detect_faces(frame)
+      # Terminate the stream after pressing 'Esc' key.
+      if cv2.waitKey(1) & 0xFF == int(27):
+        toast('Alert', 'Stream terminated.')
         disconnect(stream)
         exit(0)
       cv2.imshow('Live feed', frame)
