2739cb11b8ad51272573300d15fe869af537229a
akshayinpoint <akshay@inpointtech.com>
2020-02-12 20:26:43 +0530

    Refactor code and add functions
    * Replace os.path.basename with pathlib.
    * Add docstrings to all the functions.
    * Add create_copy() and rename_file()

--- utils/local.py
+++ utils/local.py
@@ -1,32 +1,64 @@
 """Utility for simplifying file operations."""
 
 import os
+import shutil
+from pathlib import Path
 from typing import Optional
 
+from video_processing_engine.utils.generate import hash_x
 
-def get_filename(path: str) -> str:
-    """Return filename."""
-    name = (os.path.basename(path).split('.')[0]).lower()
-    if len(name) > 10:
-        return name[:11]
-    else:
-        return name
+
+def get_file_name(path: str) -> str:
+  """Return filename."""
+  return Path(path).stem
 
 
 def get_directory_name(path: str) -> str:
-  """Get directory where the videos will be stored."""
-  return os.path.join(os.path.dirname(path), get_filename(path))
+  """Return directory where the video(s) will be stored."""
+  return os.path.join(os.path.dirname(path), get_file_name(path))
 
 
-def create_directory(path: str) -> None:
-    """Create directory with file name"""
-    if not os.path.isdir(get_directory_name(path)):
-      os.mkdir(get_directory_name(path))
+def create_directory(path: str,
+                     retval: Optional[bool] = False) -> Optional[str]:
+  """Create directory with filename and return its path.
+
+  Args:
+    path: Path where the directory needs to be created.
+    retval: Boolean value (default: False) to return directory path.
+
+  Returns:
+    Directory path is retval argument is chosen True.
+  """
+  if not os.path.isdir(get_directory_name(path)):
+    os.mkdir(get_directory_name(path))
+  if retval:
+    return get_directory_name(path)
 
 
 def filename(path: str,
-             video_number: int,
+             video_num: int,
              extension: Optional[str] = 'mp4') -> str:
+  """Creates a directory to store all files and return filename.
+
+  Args:
+    path: Path where the directory needs to be created.
+    video_num: Video number to append to the trimmed videos.
+    extension: Video file extension (default: mp4) to be used.
+
+  Returns:
+    Complete filename with the clip number and video extension.
+  """
   create_directory(path)
   return os.path.join(get_directory_name(path),
-                      f'{get_filename(path)}_{video_number}.{extension}')
+                      f'{get_file_name(path)}{hash_x(video_num)}.{extension}')
+
+
+def create_copy(path: str) -> None:
+  """Creates a copy of the video file."""
+  return shutil.copy(path, create_directory(path, True))
+
+
+def rename_file(original: str, rename: str) -> str:
+  """Returns renamed file path."""
+  os.rename(original, original.replace(Path(original).stem, rename))
+  return original.replace(Path(original).stem, rename)
