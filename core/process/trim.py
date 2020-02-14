fc44f19a64846bc64c61c3853b47125a3bd13dec
akshayinpoint <akshay@inpointtech.com>
2020-02-14 20:22:24 +0530

    Add new functions for demo - TODO: Require fixes

--- core/process/trim.py
+++ core/process/trim.py
@@ -1,7 +1,11 @@
 """A subservice for trimming the videos."""
 
-from datetime import timedelta, datetime
-from typing import Any, Optional, Union
+import os
+import shutil
+import time
+from datetime import timedelta
+from pathlib import Path
+from typing import Any, Optional, Tuple, Union
 
 from moviepy.editor import VideoFileClip as vfc
 
@@ -51,17 +55,18 @@ def trim_num_parts(source: Any,
   start = 0
 
   if local:
-    from bs3.utils.local import filename
+    from video_processing_engine.utils.local import filename
   else:
-    from bs3.utils.generate import filename
+    # TODO(xames3): Update the below module to be functional with
+    # upcoming changes.
+    from video_processing_engine.utils.generate import filename
 
-  for idx in range(num_parts):
+  for idx in range(1, num_parts + 1):
     file = filename(source, idx, choose_extension(codec))
     start, end = start, start + split_part
     trim_video(source, file, start, end, codec,
                bitrate, fps, audio, preset, threads)
     start += split_part
-    print(f'{file} is trimmed.')
 
 
 def choose_extension(codec: Optional[str] = 'libx264') -> str:
@@ -70,6 +75,42 @@ def choose_extension(codec: Optional[str] = 'libx264') -> str:
 
 
 def calc_bitrate(source: Any) -> int:
+  """Calculates the bitrate for a particular video."""
+  # You can find the reference code here:
+  # https://www.ezs3.com/public/What_bitrate_should_I_use_when_encoding_my_video_How_do_I_optimize_my_video_for_the_web.cfm
   return (vfc(source).size[0] *
           vfc(source).size[1] *
           (vfc(source).reader.nframes // vfc(source).duration) * 0.07 // 1000)
+
+
+def check_length(source: Any, minimum_length: Optional[int] = 30) -> bool:
+  """Returns boolean value after checking video length."""
+  return True if float(vfc(source).duration) >= 24 * minimum_length else False
+
+
+def random_sample_video(source: Any, sampling_rate: Optional[int] = 2) -> None:
+  """Considers a usuable sample based on sampling rate."""
+  total_length = float(vfc(source).duration)
+  desired_length = (total_length * sampling_rate) / 100
+  clipped_length = desired_length / 30
+
+
+def compress_video(source: Any,
+                   codec: Optional[str] = 'libx264',
+                   bitrate: Optional[int] = 400,
+                   fps: Optional[int] = 24,
+                   audio: Optional[bool] = False,
+                   preset: Optional[str] = 'ultrafast',
+                   threads: Optional[int] = 15) -> Tuple:
+  """Trim video in number of equal parts."""
+  temp = source.replace(Path(source).stem, 'temporary_compressed')
+  shutil.copy(source, temp)
+  trim_video(source, temp, 0, int(vfc(source).duration), codec,
+             bitrate, fps, audio, preset, threads)
+  return temp
+
+
+# sec = 10 * 3600
+# tot = (0.02 * sec)
+# eac = tot / 24
+# print(tot, eac)
