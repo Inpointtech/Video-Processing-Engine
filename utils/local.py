6b5c8218e2ec59b36beb4016df2e11dbff05c5a2
akshayinpoint <akshay@inpointtech.com>
2020-02-18 10:55:22 +0530

    Rework the local file operations
    * Add important file operations functions to simplify work.
    * Add functions which take care of creating copies of the original video.
    * Add function to rename copied file & original file.

--- utils/local.py
+++ utils/local.py
@@ -5,75 +5,75 @@ import shutil
 from pathlib import Path
 from typing import Optional
 
-from video_processing_engine.utils.generate import hash_x, hash_xa
+# TODO(xames3): Remove suppressed pyright warnings.
+# pyright: reportMissingTypeStubs=false
+from video_processing_engine.utils.generate import hash_aa
 
 
-def get_file_name(path: str) -> str:
-  """Return filename."""
-  return Path(path).stem
-
-
-def get_directory_name(path: str) -> str:
-  """Return directory where the video(s) will be stored."""
-  return os.path.join(os.path.dirname(path), get_file_name(path))
-
-
-def create_directory(path: str) -> str:
-  """Create directory with filename and return its path.
+def create_dir_with_same_filename(file: str) -> str:
+  """Create directory with same filename and return its path.
 
   Args:
-    path: Path where the directory needs to be created.
+    file: File to be used for creating directory.
 
   Returns:
-    String directory path.
+    Directory path.
   """
-  if not os.path.isdir(get_directory_name(path)):
-    os.mkdir(get_directory_name(path))
-  return get_directory_name(path)
+  directory_path = os.path.join(os.path.dirname(file), Path(file).stem)
+  if not os.path.isdir(directory_path):
+    os.mkdir(directory_path)
+  return directory_path
 
 
-def filename(path: str,
-             video_num: int,
-             extension: Optional[str] = 'mp4') -> str:
-  """Creates a directory to store all files and return filename.
+def create_copy(file: str,
+                copy_path: Optional[str] = None,
+                copy_name: Optional[str] = None) -> str:
+  """Create copy of the file and return path of the copied file.
 
   Args:
-    path: Path where the directory needs to be created.
-    video_num: Video number to append to the trimmed videos.
-    extension: Video file extension (default: mp4) to be used.
+    file: File to be copied.
+    copy_path: Path (default: None) where the copy needs to be created.
+    copy_name: Name (default: None) of the copy file.
 
   Returns:
-    Complete filename with the clip number and video extension.
+    Path where the copy is created.
   """
-  create_directory(path)
-  return os.path.join(get_directory_name(path),
-                      f'{get_file_name(path)}{hash_x(video_num)}.{extension}')
+  if copy_path is None:
+    copy_path = create_dir_with_same_filename(file)
+  if copy_name is None:
+    copy_name = os.path.basename(file)
+  return shutil.copy(file, os.path.join(copy_path, copy_name))
 
 
-def create_copy(path: str) -> None:
-  """Creates a copy of the video file."""
-  return shutil.copy(path, create_directory(path))
+def rename_original_file(file: str, bucket_name: str, order_name: str) -> str:
+  """Renames original file."""
+  new_name = file.replace(Path(file).stem, f'{bucket_name}{order_name}aaaa')
+  os.rename(file, file.replace(Path(file).stem,
+                               f'{bucket_name}{order_name}aaaa'))
+  return new_name
 
 
-def rename_file(original: str, rename: str) -> str:
-  """Returns renamed file path."""
-  os.rename(original, original.replace(Path(original).stem, rename))
-  return original.replace(Path(original).stem, rename)
+def rename_aaaa_file(file: str, video_type: str) -> str:
+  """Replaces 'aaaa' in the filename with video type sequence."""
+  temp_name = ''.join([Path(file).stem[:-4], video_type])
+  new_name = file.replace(Path(file).stem, temp_name)
+  os.rename(file, new_name)
+  return new_name
 
 
-def beta_filename(path: str,
-                  video_num: int,
-                  extension: Optional[str] = 'mp4') -> str:
-  """Creates a directory to store all files and return filename.
+def temporary_rename(file: str, rename: Optional[str] = 'temp_xa') -> str:
+  """Renames file temporarily for operation."""
+  temp = os.path.splitext(file)
+  return ''.join([temp[0], rename, temp[1]])
 
-  Args:
-    path: Path where the directory needs to be created.
-    video_num: Video number to append to the trimmed videos.
-    extension: Video file extension (default: mp4) to be used.
 
-  Returns:
-    Complete filename with the clip number and video extension.
-  """
-  create_directory(path)
-  return os.path.join(get_directory_name(path),
-                      f'{get_file_name(path)}{hash_xa(video_num)}.{extension}')
+def temporary_copy(file: str) -> str:
+  """Creates a temporary copy for operation."""
+  temp_file = temporary_rename(file)
+  return shutil.copy(file, temp_file)
+
+
+def filename(file: str, video_num: int) -> str:
+  """Returns filename with hashed index."""
+  temp_name = os.path.splitext(file)
+  return ''.join([temp_name[0], hash_aa(video_num), temp_name[1]])
