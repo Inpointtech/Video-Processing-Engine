fb4f5b638baa578b011d072caa81178254692b1c
akshayinpoint <akshay@inpointtech.com>
2020-03-02 16:27:51 +0530

    Add support for tmp file & use FFMPEG over moviepy

--- video_processing_engine/core/process/concate.py
+++ video_processing_engine/core/process/concate.py
@@ -1,58 +1,67 @@
 """A subservice for concatenating the videos."""
 
 import os
-import random
-from typing import List, Optional, Union
+from pathlib import Path
+from typing import List
 
-# TODO(xames3): Remove suppressed pyright warnings.
-# pyright: reportMissingTypeStubs=false
-from moviepy.editor import VideoFileClip as vfc, concatenate_videoclips as cvc
+from video_processing_engine.core.process.stats import video_file_extensions
 
-from video_processing_engine.utils.common import now
+
+def create_temporary_file(file: str, output: str = None) -> str:
+  """Create a temporary file for FFMPEG's further processing.
+
+  Create a temporary file for FFMPEG's PIPE based operation.
+
+  Args:
+    file: File to be converted to a temporary state.
+    output: Name (default: None) of the temporary file.
+  """
+  if output is None:
+    output = os.path.splitext(Path(file).stem)[0]
+  temp_name = os.path.join(os.path.dirname(file), output)
+  os.system(f'ffmpeg -loglevel error -y -i {file} -c copy -bsf:v '
+            f'h264_mp4toannexb -f mpegts {temp_name}.ts')
+  return f'{temp_name}.ts'
+
+
+def get_concate_list(files: List) -> str:
+  """Returns concatenated files list."""
+  concate_string = ''
+  for file in files:
+    concate_string += file + '|'
+  return concate_string[:-1]
 
 
 def concate_videos(files: List,
                    output: str,
-                   codec: Optional[str] = 'libx264',
-                   bitrate: Optional[int] = 400,
-                   fps: Optional[int] = 24,
-                   audio: Optional[bool] = False,
-                   preset: Optional[str] = 'ultrafast',
-                   threads: Optional[int] = 15,
-                   delete_old_files: Optional[bool] = True) -> str:
+                   retain_stats: bool = True,
+                   delete_old_files: bool = True) -> str:
   """Concatenates video.
 
   Concatenates videos as per the requirements.
 
   Args:
     file: List of files to be concatenated.
-    output: Path of the output file.
-    codec: Codec (default: libx264 -> mp4) to be used for concatenation.
-    bitrate: Bitrate (default: min. 400) used for the concatenation.
-    fps: FPS (default: 24) of the concatenated video.
-    audio: Boolean (default: False) value to have audio in concatenated
-           file.
-    preset: The speed (default: ultrafast) used for applying the
-            concatenation technique.
-    threads: Number of threads (default: 15) to be used for
-             concatenation.
+    output: Name of the output file.
+    retain_stats: Boolean (default: True) to use the existing stats.
     delete_old_files: Boolean (default: True) value to delete the older
                       files once the concatenation is done.
 
   Returns:
     Path where the concatenated file is created.
   """
-  videos = []
+  temp_videos = []
   if len(files) == 1:
     return files[0]
   output = os.path.join(os.path.dirname(files[0]), output)
   for file in files:
-    videos.append(vfc(file))
-  concatenated_video = cvc(videos)
-  concatenated_video.write_videofile(output, codec=codec, fps=fps, audio=audio,
-                                     preset=preset, threads=threads,
-                                     bitrate=f'{bitrate}k', logger=None)
+    temp_file = create_temporary_file(file)
+    temp_videos.append(temp_file)
+  temp_string = get_concate_list(temp_videos)
+  os.system(f'ffmpeg -loglevel error -y -i "concat:{temp_string}" -c copy '
+            f'-bsf:a aac_adtstoasc {output}')
   if delete_old_files:
+    files.extend(temp_videos)
     for file in files:
       os.remove(file)
   return output
@@ -61,13 +70,8 @@ def concate_videos(files: List,
 def concate_everything_but_live(directory: str,
                                 live_file: str,
                                 output: str,
-                                codec: Optional[str] = 'libx264',
-                                bitrate: Optional[int] = 400,
-                                fps: Optional[int] = 24,
-                                audio: Optional[bool] = False,
-                                preset: Optional[str] = 'ultrafast',
-                                threads: Optional[int] = 15,
-                                delete_old_files: Optional[bool] = True) -> str:
+                                retain_stats: bool = True,
+                                delete_old_files: bool = True) -> str:
   """Concatenates video except the live video.
 
   Concatenates all videos in the directory except the live video.
@@ -76,15 +80,7 @@ def concate_everything_but_live(directory: str,
     file: List of files to be concatenated.
     live_file: Live file to be skipped while concatenating.
     output: Path of the output file.
-    codec: Codec (default: libx264 -> mp4) to be used for concatenation.
-    bitrate: Bitrate (default: min. 400) used for the concatenation.
-    fps: FPS (default: 24) of the concatenated video.
-    audio: Boolean (default: False) value to have audio in concatenated
-           file.
-    preset: The speed (default: ultrafast) used for applying the
-            concatenation technique.
-    threads: Number of threads (default: 15) to be used for
-             concatenation.
+    retain_stats: Boolean (default: True) to use the existing stats.
     delete_old_files: Boolean (default: True) value to delete the older
                       files once the concatenation is done.
 
@@ -94,7 +90,7 @@ def concate_everything_but_live(directory: str,
   concate_files = []
   for file in os.listdir(directory):
     file = os.path.join(directory, file)
-    concate_files.append(file)
+    if os.path.splitext(file)[1] in video_file_extensions:
+      concate_files.append(file)
   concate_files.remove(live_file)
-  return concate_videos(concate_files, output, codec, bitrate, fps, audio,
-                       preset, threads, delete_old_files)
+  return concate_videos(concate_files, output, retain_stats, delete_old_files)
