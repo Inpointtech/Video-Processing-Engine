558e7b9aa3476588be5de0218b163a4208595dcf
akshayinpoint <akshay@inpointtech.com>
2020-03-04 17:40:13 +0530

    Refactor & switch to FFMPEG from moviepy
    * Refactor code & fix the docstrings.
    * Cleaned the unused imports.
    * Switched to FFMPEG from moviepy.

--- video_processing_engine/core/process/compress.py
+++ video_processing_engine/core/process/compress.py
@@ -1,53 +1,33 @@
 """A subservice for compressing the videos."""
 
-from typing import Optional, Union
+import os
+from typing import Optional
 
-# TODO(xames3): Remove suppressed pyright warnings.
-# pyright: reportMissingTypeStubs=false
-from moviepy.editor import VideoFileClip as vfc
-
-from video_processing_engine.utils.hasher import h_extension
-from video_processing_engine.core.process.stats import bitrate as b, fps as f
-from video_processing_engine.utils.local import temporary_copy
+from video_processing_engine.core.process.stats import bitrate as b
+from video_processing_engine.utils.local import quick_rename
 
 
-def compress_video(file: str,
-                   codec: str = 'libx264',
-                   bitrate:  Union[int, str] = 400,
-                   fps: Union[float, int] = 24,
-                   audio: bool = False,
-                   preset: str = 'ultrafast',
-                   threads: int = 15) -> str:
+def calculate_cbr(file: str, ratio: int) -> str:
+  """Calculates the required bitrate for compression."""
+  return str(int(b(file) * (100 - ratio)))
+
+
+def compress_video(file: str, ratio: int) -> Optional[str]:
   """Compresses video.
 
   Compresses video as per the requirements.
 
   Args:
     file: File to be compressed.
-    codec: Codec (default: libx264 -> .mp4) to be used for compression.
-    bitrate: Bitrate (default: min. 400) used for the compression.
-    fps: FPS (default: 24) of the compressed video.
-    audio: Boolean (default: False) value to have audio in compressed
-           file.
-    preset: The speed (default: ultrafast) used for applying the
-            compression technique.
-    threads: Number of threads (default: 15) to be used for compression.
+    ratio: Compression ratio to be applied.
 
   Returns:
     Path of the temporary duplicate file created.
   """
-  if b(file) < 400:
-    bitrate = 400
-  if f(file) < 24:
-    fps = 24
-  temp = temporary_copy(file)
-  video = vfc(temp, audio=audio, verbose=True)
-  video.write_videofile(file, codec=codec, fps=fps, audio=audio,
-                        preset=preset, threads=threads, bitrate=f'{bitrate}k',
-                        logger=None)
+  if ratio <= 90:
+    bitrate = calculate_cbr(file, ratio)
+  else:
+    return None
+  file, temp = quick_rename(file)
+  os.system(f'ffmpeg -loglevel error -y -i {temp} -b {bitrate} {file}')
   return temp
-
-
-def choose_extension(codec: str = 'libx264') -> str:
-    """Returns suitable file extension."""
-    return h_extension.get(codec, 'mp4')
