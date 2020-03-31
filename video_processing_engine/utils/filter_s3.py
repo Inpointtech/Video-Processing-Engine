9e26aedab77e5533d386caf7163e027bd9ac80a1
akshayinpoint <akshay@inpointtech.com>
2020-03-31 16:03:30 +0530

    Update filename length to match current scenario

--- video_processing_engine/utils/filter_s3.py
+++ video_processing_engine/utils/filter_s3.py
@@ -11,11 +11,11 @@ from video_processing_engine.utils.generate import (unhash_area_code,
 def split_filename(file_name: str) -> Optional[Tuple]:
   """Splits the hashed S3 file name into it's respective indices."""
   file_name = os.path.basename(file_name)
-  if len(file_name) == 36:
+  if len(file_name) == 34:
     return (unhash_country_code(file_name[:2]), file_name[2:6],
             file_name[6:8], file_name[8:10], file_name[10:15],
             unhash_area_code(file_name[15:16]), file_name[16:18],
-            unhash_timestamp(file_name[18:28]), file_name[28:32])
+            unhash_timestamp(file_name[18:28]), file_name[28:30])
   else:
     return None
 
