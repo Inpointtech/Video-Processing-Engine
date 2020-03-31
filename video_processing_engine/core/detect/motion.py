47aa4712f31b414a55a883bfb282b9309df8e01e
akshayinpoint <akshay@inpointtech.com>
2020-03-31 16:00:41 +0530

    Add support for logging motion metadata
    * Add support for counting bounding boxes and displaying
      timestamp of the detected motion.
    * Add support for logging the timestamps of the detected
      motions in a CSV file.

--- video_processing_engine/core/detect/motion.py
+++ video_processing_engine/core/detect/motion.py
@@ -1,9 +1,10 @@
 """A subservice for detection motion in videos."""
 
+import csv
 import logging
 import os
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
 import cv2
 import imutils
@@ -14,10 +15,17 @@ from video_processing_engine.utils.local import filename
 from video_processing_engine.utils.logs import log as _log
 from video_processing_engine.utils.opencv import (disconnect,
                                                   draw_bounding_box, rescale)
+from video_processing_engine.utils.common import seconds_to_datetime as s2d
+from video_processing_engine.vars import dev
+
+
+def motion_meta(boxes: int, occurence: Union[float, int]) -> str:
+  box = 'Bounding box' if boxes == 1 else 'Bounding boxes'
+  return f'{boxes} {box} detected at {s2d(int(occurence / 1000))}'
 
 
 def track_motion(file: str,
-                 precision: int = 2000,
+                 precision: int = 1500,
                  resize: bool = True,
                  resize_width: int = 640,
                  debug_mode: bool = True,
@@ -25,7 +33,8 @@ def track_motion(file: str,
   """Track motion in the video using Background Subtraction method."""
   log = _log(__file__) if log is None else log
   kcw = KeyClipWriter(bufSize=32)
-  consec_frames = 0
+  consec_frames, x0, y0, x1, y1 = 0, 0, 0, 0, 0
+  boxes, temp_csv_entries = [], []
   directory = os.path.join(os.path.dirname(file), Path(file).stem)
   if not os.path.isdir(directory):
     os.mkdir(directory)
@@ -69,6 +78,11 @@ def track_motion(file: str,
           kcw.start(filename(temp_file, idx),
                     cv2.VideoWriter_fourcc(*'mp4v'), fps)
           idx += 1
+        boxes.append([x1, y1])
+        status = motion_meta(len(boxes), stream.get(cv2.CAP_PROP_POS_MSEC))
+        log.info(status)
+        temp_csv_entries.append(status)
+      boxes = []
       if update_frame:
         consec_frames += 1
       kcw.update(frame)
@@ -82,6 +96,11 @@ def track_motion(file: str,
     if kcw.recording:
       kcw.finish()
     concate_temp = concate_videos(directory, delete_old_files=True)
+    with open(os.path.join(directory, f'{Path(file).stem}.csv'), 'a',
+              encoding=dev.DEF_CHARSET) as csv_file:
+      log.info('Logging detections into a CSV file.')
+      _file = csv.writer(csv_file, delimiter='\n', quoting=csv.QUOTE_MINIMAL)
+      _file.writerow(temp_csv_entries)
     if concate_temp:
       if os.path.isfile(concate_temp):
         log.info('Applying H264 encoding for bypassing browser issues.')
