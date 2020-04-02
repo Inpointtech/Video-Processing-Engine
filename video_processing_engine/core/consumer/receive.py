96c2567dd93281298384e0e1cf7c3fb72231536e
akshayinpoint <akshay@inpointtech.com>
2020-04-02 15:23:55 +0530

    Remove test bench and make main function live

--- video_processing_engine/core/consumer/receive.py
+++ video_processing_engine/core/consumer/receive.py
@@ -50,57 +50,5 @@ def consume(**kwargs):
     exit(0)
 
 
-# while True:
-#   consume(file=__file__)
-
-json_obj = {"country_code": "in",
-            "customer_id": "0555",
-            "contract_id": "44",
-            "order_id": "33",
-            "store_id": "221",
-            "camera_id": "1",
-            "area_code": "x",
-            "use_stored": False,
-            "access_mode": "s3",
-            "public_url": "",
-            "azure_account_name": "",
-            "azure_account_key": "",
-            "azure_container_name": "",
-            "azure_blob_name": "60 Minute Timer.mp4",
-            "remote_username": False,
-            "remote_password": False,
-            "remote_public_address": False,
-            "remote_file": False,
-            "s3_access_key": "",
-            "s3_secret_key": "",
-            "s3_url": "",
-            "teamviewer_file": "test.mp4",
-            "s3_bucket_name": False,
-            "start_time": "17:50:00",
-            "end_time": "17:50:30",
-            "camera_address": "203.192.197.184",
-            "camera_port": "9002",
-            "camera_username": "admin",
-            "camera_password": "user@1234",
-            "timestamp_format": False,
-            "select_sample": True,
-            "sampling_rate": "5",
-            "perform_compression": True,
-            "perform_trimming": True,
-            "trim_compressed": True,
-            "trim_type": "trim_by_factor",
-            "compression_ratio": "60",
-            "number_of_clips": "24",
-            "equal_distribution": True,
-            "clip_length": 30,
-            "trim_factor": "s",
-            "last_clip": False,
-            "sample_start_time": "17:40:00",
-            "sample_end_time": "17:45:00",
-            }
-
-import json
-
-json_obj = json.dumps(json_obj)
-
-compute(json_obj, file=__file__)
+while True:
+  consume(file=__file__)
