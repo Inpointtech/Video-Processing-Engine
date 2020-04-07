6ad644380176b0dd510abf60afec54a5d40804ea
akshayinpoint <akshay@inpointtech.com>
2020-04-07 16:12:36 +0530

    Add wrapper for pushing files to AWS Glacier

--- video_processing_engine/utils/boto_wrap.py
+++ video_processing_engine/utils/boto_wrap.py
@@ -1,14 +1,17 @@
 """Utility for work as a wrapper around Amazon's Boto3 API."""
 
 import logging
+import math
 import os
 from typing import List, Optional, Tuple
 
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
+from botocore.utils import calculate_tree_hash
 
 from video_processing_engine.utils.common import file_size as fz
 from video_processing_engine.utils.paths import downloads
+from video_processing_engine.vars import dev
 
 
 def create_s3_bucket(access_key: str,
@@ -289,3 +292,109 @@ def copy_file_from_bucket(access_key: str,
     }
     s3.meta.client.copy(copy_source, bucket_name, bucket_obj_key)
     return True
+
+
+def create_glacier_vault(access_key: str,
+                         secret_key: str,
+                         account_id: str,
+                         vault_name: str,
+                         log: logging.Logger,
+                         region: str = 'ap-south-1') -> bool:
+  """Create a S3 Glacier vault.
+
+  Create a S3 Glacier vault for archiving data in a specified region.
+  If a region is not specified, the bucket is created in the S3 default
+  region is 'ap-south-1 [Asia Pacific (Mumbai)]'.
+
+  Args:
+    access_key: AWS access key.
+    secret_key: AWS secret key.
+    account_id: AWS account ID.
+    vault_name: Vault to create.
+    log: Logger object for logging the status.
+    region: Bucket region (default: ap-south-1 [Asia Pacific (Mumbai)]).
+
+  Returns:
+    Boolean value, True if the vault is created.
+  """
+  try:
+    glacier = boto3.resource('glacier',
+                             aws_access_key_id=access_key,
+                             aws_secret_access_key=secret_key,
+                             region_name=region)
+  except (ClientError, NoCredentialsError):
+    log.error('Wrong credentials used to access the AWS account.')
+    return False
+  else:
+    glacier.Vault(account_id, vault_name).create()
+    log.info('Vault created on Amazon S3 Glacier.')
+    return True
+
+
+def upload_to_vault(access_key: str,
+                    secret_key: str,
+                    account_id: str,
+                    vault_name: str,
+                    file_name: str,
+                    log: logging.Logger = None,
+                    archive_name: str = None,
+                    region: str = 'ap-south-1') -> Optional[dict]:
+  """Upload archive to S3 Glacier.
+
+  Uploads files to S3 Glacier for archival.
+
+  Args:
+    access_key: AWS access key.
+    secret_key: AWS saccess_key: str,
+    bucket_name: Bucket to upload to.
+    file_name: Local file to upload.
+    log: Logger object for logging the status.
+    s3_name: Name (default: None) for the uploaded file.
+
+  Returns:
+    Dictionary/Response of the uploaded archived file.
+  """
+  # You can find the reference code here:
+  # https://stackoverflow.com/a/52602270
+  try:
+    glacier = boto3.client('glacier',
+                           aws_access_key_id=access_key,
+                           aws_secret_access_key=secret_key,
+                           region_name=region)
+  except (ClientError, NoCredentialsError):
+    log.error('Wrong credentials used to access the AWS account.')
+    return None
+  else:
+    if archive_name is None:
+      try:
+        archive_name = os.path.basename(file_name)
+      except FileNotFoundError:
+        log.error('File not found.')
+        return None
+    upload_chunk = 2 ** 25
+    mp_upload = glacier.initiate_multipart_upload
+    mp_part = glacier.upload_multipart_part
+    cp_upload = glacier.complete_multipart_upload
+    multipart_archive_upload = mp_upload(vaultName=vault_name,
+                                         archiveDescription=file_name,
+                                         partSize=str(upload_chunk))
+    file_size = os.path.getsize(file_name)
+    multiple_parts = math.ceil(file_size / upload_chunk)
+    with open(file_name, 'rb') as upload_archive:
+      for idx in range(multiple_parts):
+        min_size = idx * upload_chunk
+        max_size = min_size + upload_chunk - 1
+        if max_size > file_size:
+          max_size = (file_size - min_size) + min_size - 1
+        file_part = upload_archive.read(upload_chunk)
+        upload_part = mp_part(vaultName=vault_name,
+                              uploadId=multipart_archive_upload['uploadId'],
+                              range=f'bytes {min_size}-{max_size}/{file_size}',
+                              body=file_part)
+    checksum = calculate_tree_hash(open(file_name, 'rb'))
+    complete_upload = cp_upload(vaultName=vault_name,
+                                uploadId=multipart_archive_upload['uploadId'],
+                                archiveSize=str(file_size),
+                                checksum=checksum)
+    log.info(f'"{file_name}" file archived on AWS S3 Glacier.')
+    return complete_upload
