e08cd250a3390c56037752195880eac6a9c95685
akshayinpoint <akshay@inpointtech.com>
2020-04-24 15:26:38 +0530

    Add support for timezone

--- video_processing_engine/utils/logs.py
+++ video_processing_engine/utils/logs.py
@@ -3,9 +3,25 @@
 import logging
 import os
 import sys
+import time
 from pathlib import Path
 
 from video_processing_engine.utils.paths import logs as _logs
+
+
+class TimeFormatter(logging.Formatter):
+
+  def formatTime(self, record, datefmt=None):
+    convert = self.converter()
+    if datefmt:
+      if '%F' in datefmt:
+        msec = '%03d' % record.msecs
+        datefmt = datefmt.replace('%F', msec)
+      string = time.strftime(datefmt, convert)
+    else:
+      temp = time.strftime('%Y-%m-%d %H:%M:%S', convert)
+      string = '%s.%03d' % (temp, record.msecs)
+    return string
 
 
 def log(file: str, level: str = 'debug') -> logging.Logger:
@@ -22,9 +38,9 @@ def log(file: str, level: str = 'debug') -> logging.Logger:
   logger.setLevel(f'{level.upper()}')
   name = f'{Path(file.lower()).stem}.log'
   name = Path(os.path.join(_logs, name))
-  custom_format = ('%(asctime)s.%(msecs)04d    %(levelname)-8s    '
+  custom_format = ('%(asctime)s    %(levelname)-8s    '
                    '%(filename)s:%(lineno)-15s    %(message)s')
-  formatter = logging.Formatter(custom_format, '%Y-%m-%d %H:%M:%S')
+  formatter = TimeFormatter(custom_format, '%Y-%m-%d %H:%M:%S.%F %Z')
   # Create log file.
   file_handler = logging.FileHandler(os.path.join(_logs, name))
   file_handler.setFormatter(formatter)
