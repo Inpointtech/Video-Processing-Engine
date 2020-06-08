e73799115c64b45c042edd475df4e281f4691ea8
akshayinpoint <akshay@inpointtech.com>
2020-06-08 20:20:51 +0530

    Add proper support for face redaction

--- video_processing_engine/core/redact/faces.py
+++ video_processing_engine/core/redact/faces.py
@@ -3,20 +3,21 @@
 import csv
 import logging
 import os
-import time
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional
 
 import cv2
 import numpy as np
+from mtcnn import MTCNN
 
 from video_processing_engine.utils.common import seconds_to_datetime as s2d
 from video_processing_engine.utils.local import filename
 from video_processing_engine.utils.logs import log as _log
 from video_processing_engine.utils.opencvapi import draw_bounding_box, rescale
-from video_processing_engine.utils.paths import (caffemodel, frontal_haar,
-                                                 prototxt)
+from video_processing_engine.utils.paths import frontal_haar
 from video_processing_engine.vars import color, dev
+
+face_detector = MTCNN(min_face_size=20)
 
 
 def pixelate(face_roi):
@@ -41,11 +42,6 @@ def pixelate(face_roi):
   return face_roi
 
 
-def face_meta(boxes: int, occurence: Union[float, int]) -> str:
-  box = 'Face' if boxes == 1 else 'Faces'
-  return f'{boxes} {box} detected at {s2d(int(occurence / 1000))}'
-
-
 def redact_faces(file: str,
                  use_ml_model: bool = True,
                  smooth_blur: bool = True,
@@ -55,53 +51,64 @@ def redact_faces(file: str,
                  log: logging.Logger = None) -> Optional[str]:
   """Apply face redaction in video using CaffeModel."""
   log = _log(__file__) if log is None else log
-  dynamic_conf = 0.1
+
   x0, y0, x1, y1 = 0, 0, 0, 0
   boxes, temp_csv_entries = [], []
+  face_count = {}
+
   directory = os.path.join(os.path.dirname(file), f'{Path(file).stem}')
+
   if not os.path.isdir(directory):
     os.mkdir(directory)
+
   temp_file = os.path.join(directory, f'{Path(file).stem}_redact.mp4')
+
   if debug_mode:
     log.info('Debug mode - Enabled.')
+
   log.info(f'Redacting faces from "{os.path.basename(file)}".')
+
   try:
-    net = cv2.dnn.readNetFromCaffe(prototxt, caffemodel)
     stream = cv2.VideoCapture(file)
     fps = stream.get(cv2.CAP_PROP_FPS)
     width, height = (int(stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
                      int(stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
     if resize:
       width, height = resize_width, int(height * (resize_width / float(width)))
+
     save = cv2.VideoWriter(filename(temp_file, 1),
                            cv2.VideoWriter_fourcc(*'mp4v'), fps,
                            (width, height))
+
     while True:
       valid_frame, frame = stream.read()
+
       if not valid_frame:
         break
+
       if frame is None:
         break
+
       if resize:
         frame = rescale(frame, resize_width)
+
       height, width = frame.shape[:2]
+
       if use_ml_model:
-        blob = cv2.dnn.blobFromImage(frame, 1.0, (width, height),
-                                     (104.0, 177.0, 123.0))
-        net.setInput(blob)
-        detected_faces = net.forward()
-        bounding_boxes = []
-        for idx in range(0, detected_faces.shape[2]):
-          if detected_faces[0, 0, idx, 2] > dynamic_conf:
-            time.sleep(dynamic_conf)
-            coords = detected_faces[0, 0, idx, 3:7] * np.array([width, height,
-                                                                width, height])
-            bounding_boxes.append(coords.astype('int'))
-            x0, y0, x1, y1 = coords.astype('int')
-            x_bias, y_bias = (x1 - x0) * 0.04, (y1 - y0) * 0.04
-            (x0, y0), (x1, y1) = ((int(x0 + x_bias), int(y0 + y_bias)),
-                                  (int(x1 - x_bias), int(y1 - y_bias)))
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        faces = face_detector.detect_faces(rgb)
+
+        for face_idx in faces:
+            # Considering detections which have confidence score higher than the
+            # set threshold.
+          if face_idx['confidence'] > 0.75:
+            x0, y0, x1, y1 = face_idx['box']
+            x0, y0 = abs(x0), abs(y0)
+            x1, y1 = x0 + x1, y0 + y1
+
             face = frame[y0:y1, x0:x1]
+
             if debug_mode:
               draw_bounding_box(frame, (x0, y0), (x1, y1), color.red)
             try:
@@ -112,16 +119,19 @@ def redact_faces(file: str,
                 frame[y0:y1, x0:x1] = pixelate(face)
             except Exception:
               pass
-            dynamic_conf = detected_faces[0, 0, idx, 2]
-            if dynamic_conf > 0.3:
-              dynamic_conf = 0.15
+
           boxes.append([x1, y1])
-          status = face_meta(len(boxes), stream.get(cv2.CAP_PROP_POS_MSEC))
-          temp_csv_entries.append(status)
+          face_occurence = s2d(int(stream.get(cv2.CAP_PROP_POS_MSEC) / 1000))
+
+          if face_occurence not in face_count.keys():
+            face_count[face_occurence] = []
+
+          face_count[face_occurence].append(len(boxes))
       else:
         face_cascade = cv2.CascadeClassifier(frontal_haar)
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+
         for (x0, y0, x1, y1) in faces:
           if debug_mode:
             draw_bounding_box(frame, (x0, y0), (x0 + x1, y0 + y1), color.red)
@@ -137,25 +147,38 @@ def redact_faces(file: str,
           except Exception:
             pass
           boxes.append([x1, y1])
-          status = face_meta(len(boxes), stream.get(cv2.CAP_PROP_POS_MSEC))
-          temp_csv_entries.append(status)
+          face_occurence = s2d(int(stream.get(cv2.CAP_PROP_POS_MSEC) / 1000))
+
+          if face_occurence not in face_count.keys():
+            face_count[face_occurence] = []
+
+          face_count[face_occurence].append(len(boxes))
+
       boxes = []
       save.write(frame)
+
       if debug_mode:
         cv2.imshow('Video Processing Engine - Redaction', frame)
+
       if cv2.waitKey(1) & 0xFF == int(27):
         break
+
     stream.release()
     save.release()
     cv2.destroyAllWindows()
+
     with open(os.path.join(directory, f'{Path(file).stem}.csv'), 'a',
               encoding=dev.DEF_CHARSET) as csv_file:
       log.info('Logging detections into a CSV file.')
-      _file = csv.writer(csv_file, delimiter='\n', quoting=csv.QUOTE_MINIMAL)
-      _file.writerow(temp_csv_entries)
+      _file = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+      _file.writerow(['Max no. of detections per second', 'Time frame'])
+      temp_csv_entries = [(max(v), k) for k, v in face_count.items()]
+      _file.writerows(temp_csv_entries)
+
     log.info('Applying H264 encoding for bypassing browser issues.')
     os.system(f'ffmpeg -loglevel error -y -i {filename(temp_file, 1)} -vcodec '
               f'libx264 {temp_file}')
+
     return temp_file
   except Exception as error:
     log.critical(f'Something went wrong because of {error}')
